88991ef11024bacc95995c5d3d865edb935e061d
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-04-25 12:44:11 +0630

    trying to add extension

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-export {default as Carbon} from './src/Carbon';
-export {default as toHumanReadableFileSize} from './src/to-human-readable-filesize';
-export {default as Random} from './src/Random';
-export {default as Toaster} from './src/Toaster';
-export {default as String} from './src/String';
+import C from "./src/Carbon";
+export const Carbon = C;
+export { default as toHumanReadableFileSize } from "./src/to-human-readable-filesize";
+export { default as Random } from "./src/Random";
+export { default as Toaster } from "./src/Toaster";
+export { default as String } from "./src/String";
