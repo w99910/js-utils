f315df95c65c20148681ded68748e88f275084d8
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-04-25 12:45:54 +0630

    trying to add extension

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export const Carbon = require("./src/Carbon");
 export { default as toHumanReadableFileSize } from "./src/to-human-readable-filesize";
 export { default as Random } from "./src/Random";
 export { default as Toaster } from "./src/Toaster";
