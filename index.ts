0f9cd544397364cb0b57e2fa3f8df807d9763174
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-04-25 12:40:55 +0630

    trying to add extension

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-export {default as Carbon} from './src/Carbon.ts';
-export {default as toHumanReadableFileSize} from './src/to-human-readable-filesize.ts';
-export {default as Random} from './src/Random.ts';
-export {default as Toaster} from './src/Toaster.ts';
-export {default as String} from './src/String.ts';
+export {default as Carbon} from './src/Carbon';
+export {default as toHumanReadableFileSize} from './src/to-human-readable-filesize';
+export {default as Random} from './src/Random';
+export {default as Toaster} from './src/Toaster';
+export {default as String} from './src/String';
